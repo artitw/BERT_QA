97f64b02462b6abdb11d77789e7e4d790dfb3821
Artit 'Art' Wangperawong <artitw@gmail.com>
2020-01-06 18:44:34 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bert_qa",
-    version="0.0.6",
+    version="0.0.7",
     author="Artit Wangperawong",
     author_email="artitw@gmail.com",
     description="Question-Answering system using state-of-the-art pre-trained language models.",
