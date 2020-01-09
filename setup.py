f5622b999cbf6a64c73f28b0e932af407e8969e9
Artit 'Art' Wangperawong <artitw@gmail.com>
2020-01-09 17:33:50 -0500

    input pipeline auto sharding

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bert_qa",
-    version="0.0.9",
+    version="0.1.0",
     author="Artit Wangperawong",
     author_email="artitw@gmail.com",
     description="Question-Answering system using state-of-the-art pre-trained language models.",
